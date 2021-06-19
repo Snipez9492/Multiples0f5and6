5a4d0949ca6996ece6deed22cfa2efb1a870b34a
Snipez9492 <waleibk92@gmail.com>
2021-06-19 11:53:08 -0400

    Third Commit

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-
 lst = list(range(1, 100))
 
 print("Multiples of 5 and 6")
 for x in lst:
     if x % 5 == 0 and x % 6 == 0:
         print(x, end=" ")
+
+print("-" * 20)
+print("Multiples of 5 and 6, using list comprehension.")
+nums = [i for i in lst if i % 5 == 0 and i % 6 == 0]
+print(nums)
